bc4510a6e5a92f245b169b9c1b2c91ed0548dec2
Roman <online@rokonet.org>
2024-04-03 11:52:53 +0200

    init

---
+++ privatemessagereceiver/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+
+namespace privatemessagereceiver
+{
+    class Program
+    {
+
+        const string ServiceBusConnectionString = "Endpoint=sb://rokonet1.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=ueiqng+krunWq0clsD5mgMgllTlTcM/OB+ASbNQcI5Y=";
+        const string QueueName = "salesmessages";
+
+        static void Main(string[] args)
+        {
+
+            ReceiveSalesMessageAsync().GetAwaiter().GetResult();
+
+        }
+
+        static async Task ReceiveSalesMessageAsync()
+        {
+
+            Console.WriteLine("======================================================");
+            Console.WriteLine("Press ENTER key to exit after receiving all the messages.");
+            Console.WriteLine("======================================================");
+
+
+            // Create a Service Bus client that will authenticate using a connection string
+            var client = new ServiceBusClient(ServiceBusConnectionString);
+
+            // Create the options to use for configuring the processor
+            var processorOptions = new ServiceBusProcessorOptions
+            {
+                MaxConcurrentCalls = 1,
+                AutoCompleteMessages = false
+            };
+
+            // Create a processor that we can use to process the messages
+            await using ServiceBusProcessor processor = client.CreateProcessor(QueueName, processorOptions);
+
+            // Configure the message and error handler to use
+            processor.ProcessMessageAsync += MessageHandler;
+            processor.ProcessErrorAsync += ErrorHandler;
+
+            // Start processing
+            await processor.StartProcessingAsync();
+
+
+            Console.Read();
+
+            // Close the processor here
+            await processor.CloseAsync();
+
+        }
+
+        // handle received messages
+        static async Task MessageHandler(ProcessMessageEventArgs args)
+        {
+            string body = args.Message.Body.ToString();
+            Console.WriteLine($"Received: {body}");
+
+            // complete the message. messages is deleted from the queue.
+            await args.CompleteMessageAsync(args.Message);
+        }
+
+        // handle any errors when receiving messages
+        static Task ErrorHandler(ProcessErrorEventArgs args)
+        {
+            Console.WriteLine(args.Exception.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ privatemessagesender/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+
+namespace privatemessagesender
+{
+    class Program
+    {
+
+        const string ServiceBusConnectionString = "Endpoint=sb://rokonet1.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=ueiqng+krunWq0clsD5mgMgllTlTcM/OB+ASbNQcI5Y=";
+        const string QueueName = "salesmessages";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sending a message to the Sales Messages queue...");
+
+            SendSalesMessageAsync().GetAwaiter().GetResult();
+
+            Console.WriteLine("Message was sent successfully.");
+        }
+
+        static async Task SendSalesMessageAsync()
+        {
+            // Create a Service Bus client here
+            // By leveraging "await using", the DisposeAsync method will be called automatically once the client variable goes out of scope.
+            // In more realistic scenarios, you would want to store off a class reference to the client (rather than a local variable) so that it can be used throughout your program.
+
+            await using var client = new ServiceBusClient(ServiceBusConnectionString);
+
+            // Create a sender here
+            await using ServiceBusSender sender = client.CreateSender(QueueName);
+
+            try
+            {
+                // Create and send a message here
+                string messageBody = $"$10,000 order for bicycle parts from retailer Adventure Works.";
+                var message = new ServiceBusMessage(messageBody);
+                Console.WriteLine($"Sending message: {messageBody}");
+                await sender.SendMessageAsync(message);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
+
+            // Close the connection to the sender here
+            finally
+            {
+                // Calling DisposeAsync on client types is required to ensure that network
+                // resources and other unmanaged objects are properly cleaned up.
+                await sender.DisposeAsync();
+                await client.DisposeAsync();
+            }
+        }
+    }
+}
